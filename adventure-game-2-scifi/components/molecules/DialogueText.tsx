2c4b5f77e2d32e9d25b916b5d8e5cff8b8f9acef
Austin Whitbeck <AustinWhitbeckArt@gmail.com>
2022-09-25 22:43:01 -0400

    Dialogue progression buttons + logic

--- adventure-game-2-scifi/components/molecules/DialogueText.tsx
+++ adventure-game-2-scifi/components/molecules/DialogueText.tsx
@@ -1,11 +1,35 @@
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 
-const DialogueText = () => {
+interface Props {
+    dialogues: string[];
+}
+
+const DialogueText = ({ dialogues }: Props) => {
+
+    const [dialogueStep, setDialogueStep] = useState<number>(0);
+
+    const increaseStep = () => {
+        if (dialogueStep === dialogues.length - 1) {
+            return console.log('last step already, do nothing');
+        }
+        setDialogueStep(dialogueStep + 1);
+    }
+
+    const decreaseStep = () => {
+        if (dialogueStep === 0) {
+            return console.log('first step already, do nothing');
+        }
+        setDialogueStep(dialogueStep - 1);
+    }
   return (
-    <Box sx={{ border: 'solid 5px', borderColor: 'secondary'}}>
-        <Typography>Some sample text of talking</Typography>
+    <Box sx={{ border: 'solid 5px', borderColor: 'secondary', textAlign: 'center'}}>
+        <Typography>{dialogues[dialogueStep]}</Typography>
+        <Box>
+            <Button variant="contained" color='secondary' onClick={decreaseStep} disabled={dialogueStep === 0}>Prev</Button>
+            <Button variant="contained" onClick={increaseStep} disabled={dialogueStep === dialogues.length - 1}>Next</Button>
+        </Box>
     </Box>
   )
 }
--- adventure-game-2-scifi/pages/dialogue-screen.tsx
+++ adventure-game-2-scifi/pages/dialogue-screen.tsx
@@ -3,12 +3,18 @@ import React from 'react'
 import DialogueAvatars from '../components/molecules/DialogueAvatars';
 import DialogueText from '../components/molecules/DialogueText';
 
+const SampleDialogue1 = [
+    "Sentence 1",
+    "Sentence 2",
+    "Sentence 3",
+]
+
 const DialogueScreen = () => {
   return (
     <>
     <div>dialogue-screen</div>
     <DialogueAvatars />
-    <DialogueText />
+    <DialogueText dialogues={SampleDialogue1}/>
     </>
   )
 }
